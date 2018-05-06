de0e9641322e9d0a5acfc68ad533b1ddd4ca0d90
Bhsjdfhsdfn <apple@appledeMacBook-Pro-6.local>
2018-05-06 23:28:20 +0800

    about zookeeper
    version2

---
+++ student_data.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog v10.2 
+MySQL - 5.5.51 : Database - student_data
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`student_data` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `student_data`;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `age` tinyint(8) DEFAULT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`age`,`password`) values ('1','张三',11,'111111'),('2','2',3,'4'),('3e9c9187-8a6f-4b58-b835-ff42cf3d67ce','李四',12,'222222');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
